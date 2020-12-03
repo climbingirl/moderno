a242fc3ab243c1613044a2a5e8714e18a99b6237
climbingirl <isvetlanaivanova@yandex.ru>
2020-12-03 16:15:53 +0500

    added section produc-slider

--- app/js/main.js
+++ app/js/main.js
@@ -6,5 +6,12 @@ $(function(){
         readOnly: true
     });
 
+    $('.product-slider__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true,
+        arrows: false
+      });
+      
     var mixer = mixitup('.products__inner');
 });
