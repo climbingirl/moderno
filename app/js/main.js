9b4c3868bdcdd01389bd15631a722854f7b15743
climbingirl <isvetlanaivanova@yandex.ru>
2021-01-13 16:15:52 +0500

    added profile page

--- app/js/main.js
+++ app/js/main.js
@@ -43,12 +43,14 @@ $(function(){
 
     $('.product-details__tabs .tab, .settings__tabs .tab').on('click', function(event) {
         var id = $(this).attr('data-id');
-            $('.product-details__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
-            $('.product-details__tabs .tabs, .settings__tabs .tabs').find('.tab').removeClass('active');
-            $(this).addClass('active');
-            $('#'+id).addClass('active-tab').fadeIn();
-            return false;
-        });
+        $('.product-details__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-details__tabs .tabs, .settings__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#'+id).addClass('active-tab').fadeIn();
+        return false;
+    });
+
+    $('select').styler();
       
     var mixer = mixitup('.products__inner');
 });
